f3506c7339734620f7f283a11a9b35eeb32b71de
Scott Clingan <sclingan1@gmail.com>
2023-03-24 14:42:10 -0500

    Added mobile layout

--- src/form.tsx
+++ src/form.tsx
@@ -3,14 +3,14 @@ function Form() {
     return (
       <div className="form">
         <form>
-          <label htmlFor='firstName'>First Name</label>
-          <input id='firstName'></input>
-          <label htmlFor='lastName'>Last Name</label>
-          <input id='lastName'></input>
-          <label htmlFor='Email'>Email Address</label>
-          <input id='Email'></input>
-          <label htmlFor='Password'>Password</label>
-          <input id='Password'></input>
+          {/* <label htmlFor='firstName'>First Name</label> */}
+          <input id='firstName' placeholder="First Name"></input>
+          {/* <label htmlFor='lastName'>Last Name</label> */}
+          <input id='lastName' placeholder="Last Name"></input>
+          {/* <label htmlFor='Email'>Email Address</label> */}
+          <input id='Email' placeholder="Email Address"></input>
+          {/* <label htmlFor='Password'>Password</label> */}
+          <input id='Password' placeholder="Password"></input>
 
           {/* change this to a submit button? */}
           <a>Claim your free trial</a>
